8b778a0f181d433b85af6652a996c408d2027679
Tudor Morariu <morariu.tudor@gmail.com>
2026-02-12 22:35:30 +0200

    Fixed math

---
+++ AI_Docs/docs/javascripts/mathjax-config.js
@@ -0,0 +1,26 @@
+// MathJax configuration
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processRefs: true,
+  },
+  options: {
+    ignoreHtmlClass: 'highlight|hljs',
+    processHtmlClass: 'arithmatex'
+  }
+};
+
+// Auto-render KaTeX
+document.addEventListener('DOMContentLoaded', function() {
+  renderMathInElement(document.body, {
+    delimiters: [
+      {left: '$$', right: '$$', display: true},
+      {left: '$', right: '$', display: false},
+      {left: '\\[', right: '\\]', display: true},
+      {left: '\\(', right: '\\)', display: false}
+    ],
+    throwOnError: false
+  });
+});
